04d949478b5b8daef2512b933e7f4ce66fd70efa
Ricardo Manuel Lazo Vásquez <ghop.0703@gmail.com>
2020-06-03 02:17:51 -0500

    Scripts de preprocesamiento iniciales
    Scripts de preprocesamiento iniciales, aun no completamente funcionales

---
+++ Preprocesamiento/06_words_fixed_stg1.py
@@ -0,0 +1,14 @@
+import json
+import math
+
+def main():
+    #read all words in 04_words_data_s and store in memory
+
+
+
+    #process Stage 1
+    #f = freqij / maxfreq|,i
+
+
+if __name__ == "__main__":
+    main()
---
+++ Preprocesamiento/06_words_fixed_stg3.py
@@ -0,0 +1,14 @@
+import json
+import math
+
+def main():
+    #read all words in 04_words_data_s and store in memory
+
+
+
+    #process Stage 3
+    #New vector jsons
+
+
+if __name__ == "__main__":
+    main()
